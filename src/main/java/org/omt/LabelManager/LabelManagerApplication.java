6a64451fc2c124f18b70c1eb170094e4a2565d4b
Tobias Urhaug <tobiasurhaug@gmail.com>
2025-11-27 15:54:50 +0100

    Initial commit, bootstraps Spring Boot

---
+++ src/main/java/org/omt/LabelManager/LabelManagerApplication.java
@@ -0,0 +1,13 @@
+package org.omt.LabelManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabelManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/omt/LabelManager/LabelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.omt.LabelManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
